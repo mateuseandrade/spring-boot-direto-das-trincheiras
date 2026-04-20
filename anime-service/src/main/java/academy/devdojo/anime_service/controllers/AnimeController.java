3385341bc354a7151e9a86537d5604f19ecff9e3
Mateus <mateuseandrade@gmail.com>
2026-04-20 10:56:36 -0300

    add AnimeController with listAll endpoint and update application.yml for thread settings

---
+++ anime-service/src/main/java/academy/devdojo/anime_service/controllers/AnimeController.java
@@ -0,0 +1,22 @@
+package academy.devdojo.anime_service.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping("v1/animes")
+@Slf4j
+public class AnimeController {
+
+    @GetMapping
+    public List<String> listAll() throws InterruptedException{
+        log.info(Thread.currentThread().getName());
+        TimeUnit.SECONDS.sleep(1);
+        return List.of("Naruto", "One Piece", "Dragon Ball");
+    }
+}
---
+++ exercises/Exercise01.md
@@ -0,0 +1,3 @@
+Create a controller and a method that returns
+a list of anime names through the following URL:
+http://localhost:5000/v1/animes
